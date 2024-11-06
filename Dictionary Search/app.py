cac46706ce05786b5aaeff5abd6c8697cafe1d27
VaibhaRagavan <vaibha97@gmail.com>
2024-11-06 09:29:34 +0000

    Update app.py

--- Dictionary Search/app.py
+++ Dictionary Search/app.py
@@ -10,6 +10,9 @@ import requests
 import json
 import dict_search
 
+
+app = Flask(__name__)
+
 @app.route("/search", methods=["GET", "POST"])
 def search():
     if request.method == "POST":
@@ -19,3 +22,7 @@ def search():
         return render_template("search.html", response1=display)
     else:
         return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, use_debugger=False, use_reloader=False)
+
