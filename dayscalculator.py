82044f5fa5bd0bdd945b326ccd3bdc3e743961af
VaibhaRagavan <vaibha97@gmail.com>
2024-11-06 09:01:50 +0000

    Add files via upload

---
+++ dayscalculator.py
@@ -0,0 +1,15 @@
+from datetime import date,datetime
+
+
+def date_fun(d):
+    input_date = d
+    format = "%Y-%m-%d"
+    str2date=datetime.strptime(input_date,format)
+    fin_date=str2date.date()
+    return fin_date
+
+
+
+def res_days(date1 , date2):
+    diff = date1 - date2
+    return(diff.days)
