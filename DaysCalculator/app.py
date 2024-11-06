616c46c77f8aa67e0ba62131be5716a13c7e072f
VaibhaRagavan <vaibha97@gmail.com>
2024-11-06 09:18:47 +0000

    Update app.py

--- DaysCalculator/app.py
+++ DaysCalculator/app.py
@@ -7,8 +7,8 @@ def res():
     if request.method == "POST":
         date1 = request.form["first date"]
         date2 = request.form["second date"]
-        enterdate1 = dayscalculator.date_fun(date1)
-        enterdate2 = dayscalculator.date_fun(date2)
+        enterdate1 = dayscalculator.date_fin(date1)
+        enterdate2 = dayscalculator.date_fin(date2)
         result = dayscalculator.res_days(enterdate1, enterdate2)
         return render_template("date.html", calculator=result, date1=date1, date2=date2)
     else:
